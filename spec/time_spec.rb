61597bb7a23244297d296d99d581318d46fe4590
Abdul Rahimi & Toma Apostolov <student@epicodus.com>
2015-08-04 15:06:44 -0700

    takes tuesday's date and returns tuesday

--- spec/time_spec.rb
+++ spec/time_spec.rb
@@ -1,8 +1,22 @@
 require("rspec")
 require("time")
 
-describe('String#time') do
-  it("takes any date and shows what day of the week it is") do
-      expect(("8/4/2015").time()).to(eq("Tuesday"))
+
+describe('Time#sleepin') do
+  it("takes today's date and returns the day of the week") do
+    test_time = Time.new(2015, 8, 4)
+    expect(test_time.sleepin()).to(eq("Tuesday"))
   end
+
+  it("takes a saturday and returns a sleep in message") do
+    
+
+
 end
+
+# describe('String#time') do
+#   it("takes today's date and checks to see if it's Tuesday") do
+#       expect(("8/8/2015").time()).to(eq("Sleep in"))
+# end
+#
+# end
